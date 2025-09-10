4aed85ae620a59b5dc7afcceadf2fc0d93f177b1
Rick <Rick@RICKTOP>
2025-09-10 00:00:31 -0400

    Added a couple new language converters.

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# Project To-Do List
+
+This program is working and complete, but could benefit from some updates in the future.
+
+1. Improve CSS. Maybe offer a light/dark toggle and make the theme a bit more modern looking?
+2. Remove or improve the help menu; it is unattractive and difficult to read.
+3. A few more language converters to add: Greek Musical notation, Devanagari
--- src/app/language.ts
+++ src/app/language.ts
@@ -1,35 +1,86 @@
-import { unicodeGreekPrecomposedData, unicodeLatinPrecomposedData } from "./app-data";
+import { unicodeGreekCombiningData, unicodeGreekMuiscalNotationInstrumental, unicodeGreekMuiscalNotationVocal, unicodeGreekPrecomposedData, unicodeLatinCombiningData, unicodeLatinPrecomposedData } from "./app-data";
 
 /**
- * Take a string in Latin ASCII diacritical shorthand format and return a string in precomposed unicode Latin format
+ * Convert text generically using the given conversion data.
  * 
- * @param text A string in Latin ASCII shorthand format.
- * @returns A string in precomposed unicode Latin formatting.
+ * Conversion data is read in sequence, so you must sort it yourself for
+ * maximal munching.
+ * 
+ * @param text            Text to be converted
+ * @param conversionData  Conversion data to be applied
+ * @returns               Converted text.
+ */
+export function convertGeneric(text: string, conversionData: string[][]): string {
+  for (let [from, to] of conversionData) {
+    text = text.replaceAll(from, to);
+  }
+  return text
+}
+
+/**
+ * Take a string in Latin ASCII diacritical shorthand format 
+ * and return a string with precomposed diacritics.
+ * 
+ * @param text  A string in Latin ASCII shorthand format.
+ * @returns     A string in unicode precomposed characters.
  */
 export function convertShorthandToLatinPrecomposed(text: string): string {
-  for (let [from, to] of unicodeLatinPrecomposedData) {
-    text = text.replaceAll(from, to);
-  }
-  return text;
-
+  return convertGeneric(text, unicodeLatinPrecomposedData);
 }
 
 /**
- * Take a string in Greek ASCII diacritical shorthand format and return a string in precomposed unicode Greek format.
+ * Take a string in Latin ASCII diacritical shorthand format 
+ * and return a string with combining diacritics.
+ * 
+ * @param text  A string in Latin ASCII shorthand format.
+ * @returns     A string with unicode combining characters.
+ */
+export function convertShorthandToLatinCombining(text: string): string {
+  return convertGeneric(text, unicodeLatinCombiningData);
+}
+
+/**
+ * Convert Greek text generically. Essentially the same as the main generic
+ * converter, but adds a whitespace buffer to ensure that terminal sigma is
+ * converted right.
+ * 
+ * @param text            The text to be converted.
+ * @param conversionData  The conversions to apply.
+ * @returns               The converted text.
+ */
+function convertGreekGeneric(text: string, conversionData: string[][]): string {
+  return convertGeneric(text + ' ', conversionData).trim();
+}
+
+/**
+ * Take a string in Greek ASCII diacritical shorthand format 
+ * and return a string in unicode Greek with precomposed characters.
  *
  * @param text    A text in Greek ASCII shorthand formatting.
- * @returns       A text in precomposed unicode Greek formatting.
+ * @returns       A text in unicode Greek with precomposed characters.
  */
 export function convertShorthandToGreekPrecomposed(text: string): string {
-  // Add trailing whitespace to ensure that sigma is still converted 
-  // even if it is the last character in the whole text.
-  text = text + ' ';
-  for (let [from, to] of unicodeGreekPrecomposedData) {
-    text = text.replaceAll(from, to);
-  }
-  // Remove the trailing whitespace added above.
-  return text.trim();
+  return convertGreekGeneric(text, unicodeGreekPrecomposedData);
 }
+
+/**
+ * Take a string in Greek ASCII diacritical shorthand format 
+ * and return a string in unicode Greek format with combining characters.
+ * 
+ * @param text    A text in Greek ASCII shorthand formatting.
+ * @returns       A text in unicode Greek with combining characters.
+ */
+export function convertShorthandToGreekCombining(text: string): string {
+  return convertGreekGeneric(text, unicodeGreekCombiningData);
+}
+
+export function convertShorthandToGreekMusicalNotationVocal(text: string): string {
+    return convertGeneric(text, unicodeGreekMuiscalNotationVocal)
+  }
+
+export function convertShorthandToGreekMusicalNotationInstrumental(text: string): string {
+    return convertGeneric(text, unicodeGreekMuiscalNotationInstrumental)
+  }
 
 /**
  * Descriptive elements of a language converter, plus a conversion function.
@@ -46,11 +97,11 @@ export const LANGUAGES: Language[] = [
     name: 'Greek (Precomposed)',
     convert: convertShorthandToGreekPrecomposed,
     description: [
-      'Typing polytonic Greek is easy with our converter!',
+      'Typing polytonic Greek with precomposed diacritics is easy with our converter!',
       'Latin letters are simply converted to Greek letters, so "abcdefg" becomes "αβξδεφγ" and "ABCDEFG" becomes "ΑΒΞΔΕΦΓ"',
       'Diacritics can be added after vowels using special characters, so "mh=nin a)/eide qea\\" becomes "μῆνιν ἄειδε θεὰ"',
-      'Sometimes a letter can take more than one diacritic and you can enter the diacritics in any order, so that both "a/)" and "a)/" become "ἄ"',
       'The output text uses precomposed Unicode characters, so not all combinations of diacritics are valid characters! For example, a diaeresis cannot apply to an alpha, and a diaeresis cannot coexist with a breathing!',
+      "If you want more combinations, try the COMBINING diacritic converter!",
       'See the demonstration text for a complete range of diacritics available in polytonic Greek'
     ],
     demoText: `abcdefghijklmnopqrstuvwxyz
@@ -69,9 +120,9 @@ oi)wnoi=si/ te dai=ta-, Dio\\s d’ e)telei/eto boulh/`
     name: 'Latin (Precomposed)',
     convert: convertShorthandToLatinPrecomposed,
     description: [
-      "Typing Latin text with diacritics is easy with our converter!",
+      "Typing Latin text with precomposed diacritics is easy with our converter!",
       'Diacritics can be added after certain letters using special characters, so "u+ber" becomes "über", "pense/" becomes "pensé" and so on',
-      "Check the demonstration on the main page to see a few examples of characters with diacritics! This is not a complete list and more combinations are possible!",
+      "Check the demonstration on the main page to see a few examples of characters with diacritics!",
       "All vowels and some consonants can take diacritics or be modified in our shorthand, and some can even take multiple diacritics.",
       "This version of the converter uses PRECOMPOSED unicode characters, so not all combinations of diacritics are available for all characters",
       "If you want more combinations, try the COMBINING diacritic converter!"
@@ -86,5 +137,61 @@ y/ y\ y~ y@ y% y^ y* y_ y+
 k/ k$ g] c] s] s$ s@ s* m@ m*
 c! s! f! z! z!$ E! S! l!
 `
+  },
+
+  {
+    name: 'Greek (Combining)',
+    convert: convertShorthandToGreekCombining,
+    description: [
+      "Typing Greek text with combining diacritics has never been this easy!",
+      'Latin letters are simply converted to Greek letters, so "abcdefg" becomes "αβξδεφγ" and "ABCDEFG" becomes "ΑΒΞΔΕΦΓ"',
+      'Diacritics can be added after vowels using special characters, so "mh=nin a)/eide qea\\" becomes "μῆνιν ἄειδε θεὰ"',
+      'The output text uses combining diacritics compose Unicode glyphs, so you can create any combination of diacritic you want!',
+      'However, the diacritics render differently depending on the order you put them in, so you might have to rearrange them to get the look you want, e.g. "a/-" and "a-/" represent the same combination, but will be rendered ά̆ and ᾰ́  (which look different in most programs)',
+      'See the demonstration text for a complete range of diacritics available in polytonic Greek'
+    ],
+    demoText: `[le]uka=n mainome/n[oisin e)sa_/lata p]o@s@s@i@n@ a)f' i(/[p]p@w@[n]`
+  },
+
+  {
+    name: 'Latin (Combining)',
+    convert: convertShorthandToLatinCombining,
+    description: ["Typing Latin text with combining diacritics has never been this easy!",
+      'Diacritics can be added after certain letters using special characters, so "u+ber" becomes "über", "pense/" becomes "pensé" and so on',
+      "Check the demonstration on the main page to see a few examples of characters with diacritics!",
+      'The output text uses combining diacritics, so you can make any combination of diacritics you want!',
+      'However, you might notice that the order causes the diacritics to render differently, so you might have to play around a bit.',
+      "Sometimes, combining diacritics don't render correctly. In these situations, try our COMBINING diacritics converter!"
+    ],
+    demoText: `a/ a\ a~ a= a^ a+ a@ a#o a&o a* a( a) a] a- a_ a|
+
+i/ i\ i~ i= i^ i+ i@ i#o i&o i* i( i) i] i- i_ i|
+
+a/) a)/ i-/ i/- a*]
+
+g/ g^ k/ h~ m@ n] t]`
+  },
+
+  {
+    name: "Greek Musical Notation (Vocal)",
+    convert: convertShorthandToGreekMusicalNotationVocal,
+    description: [""],
+    demoText: `G G+ G++ A A+ A++ B B+ B++ c c+ c++ d d+ d++ 
+e e+ e++ f f+ f++ g g+ g++ a a+ a++ b b+ b++
+c' c'+ c'++ d' d'+ d'++ e' e'+ e'++ f' f'+ f'++ g' g'+ g'++
+a' a'+ a'++ b' b'+ b'++ c'' c''+ c''++ d'' d''+ d''++ e'' e''+ e''++
+f'' f''+ f''++ g''
+`
+  },
+
+  {
+    name: "Greek Musical Notation (Instrumental)",
+    convert: convertShorthandToGreekMusicalNotationInstrumental,
+    description: [""],
+    demoText: `G G+ G++ A A+ A++ B B+ B++ c c+ c++ d d+ d++ 
+e e+ e++ f f+ f++ g g+ g++ a a+ a++ b b+ b++
+c' c'+ c'++ d' d'+ d'++ e' e'+ e'++ f' f'+ f'++ g' g'+ g'++
+a' a'+ a'++ b' b'+ b'++ c'' c''+ c''++ d'' d''+ d''++ e'' e''+ e''++
+f'' f''+ f''++ g'' `
   }
 ];
